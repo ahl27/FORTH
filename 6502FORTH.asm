4e8481288f3338510f9f3d2bebe912aef8c26373
ahl27 <ahl27@pitt.edu>
2022-12-15 10:54:05 -0500

    adds next, dolist, exit words

--- 6502FORTH.asm
+++ 6502FORTH.asm
@@ -30,18 +30,18 @@
 ;;;
 
 ;;; 2 byte variables
-IP=$0050                   ; Forth instruction pointer
-RP=$0052                   ; return stack pointer
-DT=$0054                   ; pointer to top of dictionary stack
-TMP1=$0056                 ; temp value
-TMP2=$0058                 ; temp value
+IP=$50                    ; Forth instruction pointer
+RP=$52                    ; return stack pointer
+DT=$54                    ; pointer to top of dictionary stack
+TMP1=$56                  ; temp value
+TMP2=$58                  ; temp value
 
 ;; 1 byte variables
-TPTR=$005A
-TCNT=$005B
+TPTR=$5A
+TCNT=$5B
 
 ;; other
-DPTR=$005C
+DPTR=$5C
 INPUT=$7F00                ; input space
 WORDSPC=$7EC0              ; temp space for parsing words (<=63 chars)
 
@@ -78,6 +78,15 @@ initstart:
 ;;;
 ;;; Dictionary
 ;;;
+;;; Each entry contains up to 5 values:
+;;; - 1 byte w/ upper three bits corresponding to tags, 
+;;;     lower 5 corresponding to word length
+;;; - characters comprising the word definition name
+;;; - pointer to next entry
+;;; - "code word": address of coding handling this instruction
+;;;     (usually DOLIST for compiled words)
+;;; - parameter space
+;;;     (usually list of addresses to execute this word, ending w/ EXIT)
 
 ;;; TODO, just have one entry as a placeholder
 d0entry:
@@ -87,6 +96,124 @@ d0link:
   WORD $0000
 d0code:
   WORD exit
+
+
+;;; Test Dictionary Entries go here
+doquitword
+  BYTE 0
+doquitlink
+  WORD $0000
+doquitcode
+  WORD interploop
+
+dummy
+  BYTE 0
+dummylink
+  WORD $0000
+dummycode
+  WORD dolist      ; won't actually run this, start with NEXT instead
+dummyparam
+  WORD $0000       ; will write in the actual code link word here
+  WORD doquitcode
+
+;;;
+;;; Inner Interpreter
+;;;
+;;; This section implements the core routines NEXT, DOLIST, and EXIT
+;;;
+;;; NEXT moves from one instruction to the next inside a defined word
+;;; This will be called in *every* assembly language routine.
+;;;   - increments instruction pointer
+;;;   - loads location pointed to by IP
+;;;   - jumps to that location
+;;;
+;;; DOLIST begins execution of a compiled word.
+;;;   - stores instruction pointer on return stack
+;;;   - resets instruction pointer to new word
+;;;   - calls NEXT to start executing new word
+;;;
+;;; EXIT is the last address of all compiled words.
+;;;   - undoes what DOLIST did
+;;;   - jumps back to earlier execution context
+;;;
+;;; Stack pointed to by RP keeps track of these jumps, none are subroutines
+
+;;; DOLIST Definition ;;;
+dolist:
+  ;; First, we store the instruction pointer on the return stack
+  lda IP
+  sta (RP)
+  inc RP
+  lda IP+1
+  sta (RP)
+  inc RP
+
+  ;; Next, we get the address stored at the location in IP 
+  ;; (double indirect access) and store in IP
+  lda (IP)
+  sta TMP1
+  ldy #1
+  lda (IP),y
+  sta IP+1
+  lda TMP1
+  sta IP
+
+  ;; IP now points to the code word of defined word we want
+  ;; to execute, so we can just fall through to next
+  ; jmp next
+
+;;; ENSURE DOLIST FALLS THROUGH TO NEXT!!
+
+;;; NEXT Definition ;;;
+next:
+  ;; finding the location of the next word
+  ;; we advance 2 bytes
+  ;; bne skips upper byte if we haven't rolled over
+  inc IP
+  bne .cont1
+  inc IP+1
+.cont1:
+  inc IP
+  bne .cont2
+  inc IP+1
+.cont2:
+  ;; IP now points at location of next word to execute.
+  ;; We need to fetch that location, then store it in TMP1.
+  ldy #0 
+  lda (IP),y
+  sta TMP1
+  iny
+  lda (IP),y
+  sta TMP1+1
+
+  ;; Now we load the code address stored in TMP1 into TMP2
+  lda (TMP1),y
+  sta TMP2+1
+  dey
+  lda (TMP1)
+  sta TMP2
+
+  ;; Finally, we jump to the address stored in TMP2
+  jmp (TMP2)  
+
+
+;;; EXIT Definition ;;;
+exit:
+  ;; remove an address from return stack
+  dec RP
+  dec RP
+
+  ;; Take the value that was on return stack
+  ;; and replace it in instruction pointer
+  ldy #1
+  lda (RP),y
+  sta IP+1
+  lda (RP)
+  sta IP
+
+  ;; Finally, execute the next instruction
+  jmp next
+
 
 
 ;;; Test code will go here
