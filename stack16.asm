07e1ea281a7ae94b734fab618bfde7b2556b589e
ahl27 <ahl27@pitt.edu>
2023-01-06 12:18:12 -0500

    added division and modulo for 16 bit numbers

--- stack16.asm
+++ stack16.asm
@@ -185,4 +185,162 @@ mult16:
   inx
   rts
 
+;;;
+;;; 16-bit division
+;;; remainder will also be stored at top of stack
+;;; Top element treated as divisor, bottom as numerator
+;;;
+div16withmod:
+  ;; Max iterations is 16 = 0x10, since we have 16 bit numbers
+  phy
+  ldy #$10 
 
+  ;; add two spaces on stack
+  dex
+  dex
+  dex
+  dex
+
+  stz stackbase+1,x             ; remainder
+  stz stackbase+2,x
+  stz stackbase+3,x             ; quotient
+  stz stackbase+4,x
+                                ; +5-6 is denominator
+                                ; +7-8 is numerator
+
+  ;; Set up the numerator
+  .(
+    lda #0
+    ora stackbase+8,x
+    ora stackbase+7,x
+    beq earlyexit
+
+    ;; checking is denominator is zero (if so we'll just store zeros)
+    lda #0
+    ora stackbase+6,x
+    ora stackbase+5,x
+    bne loop
+
+    earlyexit:
+      ;; Numerator or denominator are zero, just return
+      stz stackbase+6,x
+      stz stackbase+5,x
+      inx
+      inx
+      inx
+      inx
+      ply
+      rts
+
+    ;; Trim down to leading bit
+    loop:
+      lda stackbase+8,x
+      bit #%10000000            ; test upper bit
+      bne end
+      clc
+      asl stackbase+7,x
+      rol stackbase+8,x
+      dey
+      jmp loop
+    end:
+  .)
+
+  ;; Main division loop
+  .(
+    loop:
+      ;; Left-shift the remainder
+      clc
+      asl stackbase+1,x         
+      rol stackbase+2,x
+
+      ;; Left-shift the quotient
+      clc
+      asl stackbase+3,x
+      rol stackbase+4,x
+
+
+      ;; Set least significant bit to bit i of numerator
+      lda stackbase+1,x
+      clc
+      asl stackbase+7,x
+      rol stackbase+8,x
+      adc #0
+      sta stackbase+1,x
+      lda stackbase+2,x
+      adc #0
+      sta stackbase+2,x
+
+      ;; Compare remainder to denominator
+      ; upper byte (stackbase+2 is already in A)
+      cmp stackbase+6,x
+      bmi skip                  ; if R < D, skip to next iteration 
+      bne subtract              ; if R > D, we can skip comparing lower byte
+                                ; if R = D, we have to check the lower byte   
+                                
+      ; lower byte
+      lda stackbase+1,x
+      cmp stackbase+5,x
+      bmi skip
+
+    subtract:
+      ;; Subtract denominator from remainder
+      sec
+      ; subtract lower byte
+      lda stackbase+1,x
+      sbc stackbase+5,x
+      sta stackbase+1,x
+
+      ; subtract upper byte
+      lda stackbase+2,x
+      sbc stackbase+6,x
+      sta stackbase+2,x
+
+      ;; Add one to quotient
+      inc stackbase+3,x
+
+
+    skip:
+      dey
+      beq exit
+      jmp loop
+
+    exit:  
+  .)
+
+  ;; Cleanup
+  lda stackbase+1,x
+  sta stackbase+5,x
+  lda stackbase+2,x
+  sta stackbase+6,x
+
+  lda stackbase+3,x
+  sta stackbase+7,x
+  lda stackbase+4,x
+  sta stackbase+8,x
+
+  inx
+  inx
+  inx
+  inx
+  ply
+  rts
+
+
+;;;
+;;; Division helper functions
+;;;
+div16:
+  jsr div16withmod
+  inx
+  inx
+  rts
+
+mod16:
+  jsr div16withmod
+  lda stackbase+1,x
+  sta stackbase+3,x
+  lda stackbase+2,x
+  sta stackbase+4,x
+  inx
+  inx
+  rts
--- stacktest.asm
+++ stacktest.asm
@@ -19,9 +19,16 @@ main
 
   ;; multiplication
   ;; running all three multtests should store 0x0050, 0x7008, 0x1E08
-  jsr multtest1
-  jsr multtest2
-  jsr multtest3         
+  ;jsr multtest1
+  ;jsr multtest2
+  ;jsr multtest3   
+
+  ;; division with remainder 
+  ;; running all three tests should store 0x00A1, 0x008A, 0x001A     
+  jsr divtest1
+  jsr divtest2
+  jsr modtest
+
   brk
 
 
@@ -73,7 +80,7 @@ multtest2:            ; 0x04AB * 0x0018 = 0x7008, this test has rollover
   jsr mult16
   rts
 
-multtest3:            ; 0x04AB * 0x0A18 = 0x2F1E08 = 0x1E08, this test has rollover and overflows
+multtest3:            ; 0x04AB * 0x0A18 = 0x002F1E08 = 0x1E08, this test has rollover and overflows
   lda #$AB
   sta stackaccess
   lda #$04
@@ -85,6 +92,46 @@ multtest3:            ; 0x04AB * 0x0A18 = 0x2F1E08 = 0x1E08, this test has rollo
   sta stackaccess+1
   jsr push16
   jsr mult16
+  rts
+
+divtest1:             ; 0x0A10 / 0x0010 = 0x00A1, Remainder 0x0000
+  lda #$10
+  sta stackaccess
+  lda #$0A
+  sta stackaccess+1
+  jsr push16
+
+  lda #$10
+  sta stackaccess
+  lda #$00
+  sta stackaccess+1
+  jsr push16
+  jsr div16
+  rts
+
+
+divtest2:             ; 0x8A1A / 0x0100 = 0x008A, Remainder 0x001A
+  jsr divmodsetup
+  jsr div16
+  rts
+
+modtest:              ; Same as divtest2, should save remainder
+  jsr divmodsetup
+  jsr mod16
+  rts
+
+divmodsetup:
+  lda #$1A
+  sta stackaccess
+  lda #$8A
+  sta stackaccess+1
+  jsr push16
+
+  lda #$00
+  sta stackaccess
+  lda #$01
+  sta stackaccess+1
+  jsr push16
   rts
 
 pushzero
